40232fe6011b46d095320f33c836bc64ff7a2298
Yorkya <Yorkya@noreply.gitcode.com>
2025-10-18 21:46:49 +0800

    修改run_test

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,7 @@ import pytest
 if __name__ == '__main__':
     pytest.main([
         "-s", "-v",
+        "testcases",              
         "--alluredir=reports/allure-results"
     ])
     os.system("allure generate reports/allure-results -o reports/allure-report --clean")
